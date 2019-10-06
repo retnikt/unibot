d310123a5bf26172fcf4e215db4ba6dd3cef5c71
retnikt <_@retnikt.uk>
2019-10-06 10:59:58 +0100

    python version check on launch

--- unibot/__main__.py
+++ unibot/__main__.py
@@ -1,5 +1,9 @@
-from unibot.bot import Bot
+import sys
+
+assert sys.version_info >= (3, 7), "unsupported python version! please use 3.7+"
 
 if __name__ == "__main__":
+    from unibot.bot import Bot
+
     bot = Bot()
     bot.run()
